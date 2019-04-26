18e334b831419ca16417422b0559530a3d21bb4f
Jud9X <42975667+Jud9X@users.noreply.github.com>
2019-04-26 10:43:51 +0100

    Add files via upload

--- GameRecord.java
+++ GameRecord.java
@@ -13,6 +13,12 @@ public class GameRecord implements Serializable {
     private String winnerUsername;
     private String loserUsername;
     
+    /**
+     * Creates a new record of the game.
+     * @param finish The time the game ended.
+     * @param winner The User object of the winner.
+     * @param loser The User object of the loser.
+     * */
     public GameRecord(ZonedDateTime finish, User winner, User loser) {
         dateCompleted = finish;
         this.winner = winner;
@@ -21,22 +27,42 @@ public class GameRecord implements Serializable {
         loserUsername = loser.getUsername();
     }
     
+    /**
+     * Gets the date the game finished.
+     * @return The date the game finished.
+     * */
     public ZonedDateTime getDateCompleted() {
         return dateCompleted;
     }
     
+    /**
+     * Gets the winner.
+     * @return The winner of the game as a User object.
+     * */
     public User getWinner() {
         return winner;
     }
     
+    /**
+     * Gets the loser.
+     * @return The loser of the game as a User object.
+     * */
     public User getLoser() {
         return loser;
     }
     
+    /**
+     * Gets the winner's username.
+     * @return The winner of the game's username.
+     * */
     public String getWinnerUsername() {
         return winnerUsername;
     }
     
+    /**
+     * Gets the loser's username.
+     * @return The loser of the game's username.
+     * */
     public String getLoserUsername() {
         return loserUsername;
     }
